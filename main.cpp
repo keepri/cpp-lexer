8c662c6b23890fb5e532c6f6c30885c04c4ec69d
KIPRI <hi@kipri.dev>
2023-10-29 21:44:31 +0200

    feat: updated logger logic

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    Log& log= Log::get();
-    log.info("Hello, world!");
+    Log& log = Log::get();
+    log.set_level(Log::DEBUG);
+    log.debug("Hello, world!");
 
     return 0;
 }
