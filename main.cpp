7114ef8817ce2141b95027a3f55a8e3d4d3de671
KIPRI <hi@kipri.dev>
2023-11-26 20:50:17 +0200

    chore: updated logs

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    Log &log = Log::get().set_level(Log::INFO);
+    Log &log = Log::get().set_level(Log::SILLY);
     Lexer *lexer = new Lexer;
 
     Lexer::TokenVec token_vec;
